ec1f46cc3066a6feafd013d18c2b5c7ea4a93289
Jeremias <jerefigueiredo@gmail.com>
2021-02-21 19:25:34 -0300

    final commit

---
+++ client/src/components/pages/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Router } from "@reach/router";
+import NotFound from "./pages/NotFound.js";
+import LoginButton from "./pages/LoginButton.js";
+import "../utilities.css";
+import { socket } from "../client-socket.js";
+import { get, post } from "../utilities";
+import "./pages/Login-Page.css"
+
+/**
+ * Define the "App" component as a class.
+ */
+class App extends Component {
+ 
+// makes props available in this component
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: undefined,
+    };
+  }
+
+  componentDidMount() {
+    get("/api/whoami").then((user) => {
+      if (user._id) {
+        // they are registed in the database, and currently logged in.
+        this.setState({ userId: user._id });
+      }
+    });
+  }
+
+  handleLogin = (res) => {
+    console.log(`Logged in as ${res.profileObj.name}`);
+    const userToken = res.tokenObj.id_token;
+    const ProtectedComponent = () => {
+      if (authFails){
+        return <Redirect to='/dash'  />
+      }
+      return <div> My Protected Component </div>
+    }
+    post("/api/login", { token: userToken }).then((user) => {
+      this.setState({ userId: user._id });
+      post("/api/initsocket", { socketid:this.user._id });
+
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({ userId: undefined });
+    post("/api/logout");
+  };
+
+  render() {
+    return (
+      <> 
+      <div className="title">
+        <h1>hello</h1>
+        <h1>Welcome to Classdawdco!</h1>
+        <p>Education done different</p>
+      </div>
+      <div className="bttn">
+        <Router>
+          <LoginButton 
+            path="/"
+            handleLogin={this.handleLogin}
+            handleLogout={this.handleLogout}
+            userId={this.state.userId}
+          />
+          <NotFound default />
+        </Router>
+        {/*here we should have a link for each class depending on the user id*/}
+      </div>
+      </>
+ 
+    );
+  }
+}
+
+export default App;
